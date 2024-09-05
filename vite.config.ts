85820899fa2e7e96e1e1579a6abf498036b7b5ae
Alicia Sykes <alicia@omg.lol>
2024-09-05 21:10:24 +0100

    ft: Starts settings route

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,7 @@ export default defineConfig( ({ mode }) => {
             '/login',
             '/domains',
             '/about',
+            '/settings',
             '/*',
           ],
           sitemap: {
