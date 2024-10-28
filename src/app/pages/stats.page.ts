9ef17b15e862bd8cb4bf6e0108931945f8724b87
Alicia Sykes <alicia@omg.lol>
2024-10-28 00:00:23 +0000

    feat: Stats page shows current highlighted tab

--- src/app/pages/stats.page.ts
+++ src/app/pages/stats.page.ts
@@ -1,4 +1,4 @@
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { PrimeNgModule } from '../prime-ng.module';
 import { CommonModule } from '@angular/common';
@@ -14,10 +14,21 @@ import { statsLinks } from '@/app/constants/navigation-links';
 })
 export default class StatsIndexPage implements OnInit {
   items: MenuItem[] | undefined;
-  constructor() {}
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.items = statsLinks;
+  }
+
+  isActive(link: string): boolean {
+    const current = this.router.url;
+    if (current === link) {
+      return true;
+    }
+    return false;
   }
 }
 
