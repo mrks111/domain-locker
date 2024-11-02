1c6a50c1c7fee41409cf2890bdefcbe7e8823449
Alicia Sykes <alicia@omg.lol>
2024-11-02 20:05:34 +0000

    feat: Updates stats index page

--- src/app/pages/stats.page.ts
+++ src/app/pages/stats.page.ts
@@ -1,5 +1,5 @@
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { PrimeNgModule } from '../prime-ng.module';
 import { CommonModule } from '@angular/common';
 import { MenuItem } from 'primeng/api';
@@ -10,25 +10,58 @@ import { statsLinks } from '@/app/constants/navigation-links';
   imports: [CommonModule, RouterOutlet, PrimeNgModule],
   selector: 'stats-index-page',
   templateUrl: './stats/index.page.html',
-  styles: ['']
+  styles: ['::ng-deep .content-container { max-width: 1600px; }']
 })
-export default class StatsIndexPage implements OnInit {
+export default class StatsIndexPage implements OnInit, OnDestroy {
   items: MenuItem[] | undefined;
+  hideSideBar = false;
+  @ViewChild('sidebarNav', { static: false }) sidebarNav!: ElementRef;
+  hideTextLabels = false;
+  private resizeObserver!: ResizeObserver;
+
   constructor(
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
     this.items = statsLinks;
+
+    // Toggle sidebar based on the URL route
+    this.hideSideBar = this.router.url === '/stats';
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.hideSideBar = event.urlAfterRedirects === '/stats';
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    // Set up ResizeObserver to monitor the sidebar width
+    this.resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        if (entry.contentRect.width < 150) {
+          this.hideTextLabels = true;
+        } else {
+          this.hideTextLabels = false;
+        }
+        this.cdr.detectChanges(); // Trigger change detection for hideTextLabels
+      }
+    });
+    if (this.sidebarNav) {
+      this.resizeObserver.observe(this.sidebarNav.nativeElement);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.resizeObserver && this.sidebarNav) {
+      this.resizeObserver.unobserve(this.sidebarNav.nativeElement);
+      this.resizeObserver.disconnect();
+    }
   }
 
   isActive(link: string): boolean {
-    const current = this.router.url;
-    if (current === link) {
-      return true;
-    }
-    return false;
+    return this.router.url === link;
   }
 }
-
