12593ff45df5b70307dba0d23e01dcc1b36d23c9
Alicia Sykes <alicia@omg.lol>
2025-01-04 14:02:55 +0000

    ref: Moves pricing and feature stuff into own components for reusability

--- src/app/pages/(home).page.ts
+++ src/app/pages/(home).page.ts
@@ -19,6 +19,10 @@ import { EppStatusChartComponent } from '@components/charts/domain-epp-status/do
 import { DomainGanttChartComponent } from '@components/charts/registration-lifespan/registration-lifespan.component';
 import { TagGridComponent } from '@components/tag-grid/tag-grid.component';
 import { SponsorMessageComponent } from '@components/sponsor-thanks/sponsor-thanks.component';
+import { FeatureCarouselComponent } from '@components/home-things/feature-carousel/feature-carousel.component';
+import { FeaturesGridComponent } from '@components/home-things/feature-grid/feature-grid.component';
+import { PricingCardsComponent } from '@/app/components/home-things/pricing-cards/pricing-cards.component';
+import { CtaComponent } from '@components/home-things/cta/cta.component';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { ErrorHandlerService } from '@/app/services/error-handler.service';
@@ -41,6 +45,10 @@ import { ErrorHandlerService } from '@/app/services/error-handler.service';
     TagGridComponent,
     TranslateModule,
     SponsorMessageComponent,
+    FeatureCarouselComponent,
+    FeaturesGridComponent,
+    PricingCardsComponent,
+    CtaComponent,
   ],
   templateUrl: './home.page.html',
   styles: [`
