79d3085d57a5c89abf2e79acb0dbb2142bf15ca5
Alicia Sykes <alicia@omg.lol>
2025-02-16 22:47:22 +0000

    i18n: Translate hero and emo component

--- src/app/components/home-things/demo/demo.component.ts
+++ src/app/components/home-things/demo/demo.component.ts
@@ -1,36 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from '~/app/prime-ng.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-demo',
   template: `
   <div class="flex flex-wrap md:flex-nowrap w-full px-2 h-full gap-4">
     <div class="p-card flex-1 py-4 px-3">
-      <h4>Live Demo</h4>
+      <h4>{{ 'HOME.DEMO.LIVE_DEMO_TITLE' | translate }}</h4>
       <p>
-        A live demo is deployed to
-        <a href="https://demo.domain-locker.com" class="text-primary">demo.domain-locker.com</a>.
+        {{ 'HOME.DEMO.LIVE_DEMO_DESC' | translate }}
+        <a href="https://demo.domain-locker.com" class="text-primary">{{ 'HOME.DEMO.DEMO_DOMAIN' | translate }}</a>.
       </p>
-      <h5>Credentials</h5>
-        <ul class="m-0 pl-3">
-          <li>Username: <code>demo&#64;domain-locker.com</code></li>
-          <li>Password: <code>domainlocker</code></li>
-        </ul>
-        <a href="https://demo.domain-locker.com">
-          <p-button label="Visit Demo" class="float-right" icon="pi pi-desktop"></p-button>
-        </a>
+      <h5>{{ 'HOME.DEMO.CREDENTIALS' | translate }}</h5>
+      <ul class="m-0 pl-3">
+        <li>{{ 'HOME.DEMO.USERNAME' | translate }}</li>
+        <li>{{ 'HOME.DEMO.PASSWORD' | translate }}</li>
+      </ul>
+      <a href="https://demo.domain-locker.com">
+        <p-button [label]="'HOME.DEMO.VISIT_DEMO' | translate" class="float-right" icon="pi pi-desktop"></p-button>
+      </a>
     </div>
     <div class="p-card flex-1 py-4 px-3">
-      <h4>Video Demo</h4>
-      <p>TODO</p>
+      <h4>{{ 'HOME.DEMO.VIDEO_DEMO_TITLE' | translate }}</h4>
+      <p>{{ 'HOME.DEMO.VIDEO_DEMO_DESC' | translate }}</p>
       <a href="#">
-        <p-button label="Watch Video" class="float-right" icon="pi pi-video"></p-button>
+        <p-button [label]="'HOME.DEMO.WATCH_VIDEO' | translate" class="float-right" icon="pi pi-video"></p-button>
       </a>
     </div>
   </div>
-`,
+  `,
   standalone: true,
-  imports: [CommonModule, PrimeNgModule]
+  imports: [CommonModule, PrimeNgModule, TranslateModule]
 })
 export class DemoComponent  {}
