ad3396b324e6c52dfcf87a6d88bffd0473fc786a
Alicia Sykes <alicia@omg.lol>
2025-02-20 21:58:48 +0000

    feat: About page heading

--- src/app/pages/about/data/about-page-list.ts
+++ src/app/pages/about/data/about-page-list.ts
@@ -9,6 +9,7 @@ export interface AboutLink {
 
 export interface AboutPage {
   title: string;
+  description?: string;
   dirSlug?: string;
   svgIcon?: string;
   links: AboutLink[];
@@ -18,6 +19,7 @@ export const aboutPages: AboutPage[] = [
   {
     title: 'Intro',
     svgIcon: 'intro',
+    description: 'Welcome to Domain Locker, the domain management system for everyone',
     links: [
       {
         title: 'Why?',
@@ -35,6 +37,7 @@ export const aboutPages: AboutPage[] = [
   {
     title: 'Articles',
     svgIcon: 'articles',
+    description: 'In-depth articles to help you get the most out of Domain Locker',
     links: [
       { title: 'Useful tools and resources', description: 'Free and/or open source tools, utils and services for managing domains', link: '/about/external-tools', icon: '' },
       { title: 'What is a domain name', description: '', link: '', icon: '' },
@@ -45,6 +48,7 @@ export const aboutPages: AboutPage[] = [
   },
   {
     title: 'Community',
+    description: 'Domain Locker is built by the community, for the community',
     svgIcon: 'community',
     links: [
       { title: 'Attributions', description: 'Shout outs to everyone whose made Domain Locker possible', link: '/about/attributions', icon: 'pi pi-heart' },
@@ -55,6 +59,7 @@ export const aboutPages: AboutPage[] = [
   {
     title: 'Support',
     svgIcon: 'support',
+    description: 'We\'re here to help you get the most out of Domain Locker',
     links: [
       { title: 'How-Tos', description: 'Short guides to help you mae the most of Domain Locker', link: '/about', icon: '' },
       { title: 'FaQ', description: 'Answers to commonly asked questions', link: '/about/faq', icon: 'pi pi-question-circle' },
@@ -65,6 +70,7 @@ export const aboutPages: AboutPage[] = [
     title: 'Legal',
     dirSlug: 'legal',
     svgIcon: 'legal',
+    description: 'The legal stuff you need to know about using Domain Locker',
     links: [
       // These pages are auto-populated from the /src/content/docs/legal/*.md files
       // Files include: accessibility, community-guidelines, cookies, gdpr-statement,
@@ -75,11 +81,13 @@ export const aboutPages: AboutPage[] = [
     title: 'Developing',
     dirSlug: 'developing',
     svgIcon: 'developing',
+    description: 'Resources for developers who want to contribute to Domain Locker',
     links: [], // Auto-populated from /src/content/docs/developing/*.md files
   },
   {
     title: 'External Links',
     svgIcon: 'externalLinks',
+    description: 'Other stuff, like our source code, download links and more resources',
     links: [
       { title: 'GitHub', description: 'Domain Locker source code', href: 'https://github.com/lissy93/domain-locker', icon: 'pi pi-github' },
       { title: 'DockerHub', description: 'Docker container for self-hosting', href: 'https://hub.docker.com/r/lissy93/domain-locker', icon: '' },
